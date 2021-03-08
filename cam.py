35ae9dac6704713125dfbd2fac8726676e766bcc
KettleRunner <crousetech@gmail.com>
2021-03-08 15:02:46 -0600

    Create cam.py

---
+++ cam.py
@@ -0,0 +1,24 @@
+import cv2 
+import numpy as np
+
+
+video = cv2.VideoCapture(0) 
+
+   
+size = (frame_width, frame_height)
+background = None
+
+while(True): 
+    ret, frame = video.read() 
+    if ret == True:  
+        cv2.imshow('Cam', frame)
+                      
+        if cv2.waitKey(1) & 0xFF == ord('s'): 
+            break
+    else: 
+        break
+  
+ 
+video.release() 
+result.release() 
+cv2.destroyAllWindows() 
