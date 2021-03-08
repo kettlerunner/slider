c552c3a0a0edd3d6f047b1d8b7e10efb268378b0
KettleRunner <crousetech@gmail.com>
2021-03-08 15:07:58 -0600

    Update cam.py

--- cam.py
+++ cam.py
@@ -4,6 +4,17 @@ import numpy as np
 
 video = cv2.VideoCapture(0) 
 
+url = "https://dancrouse.com/slider"
+local_base = "images/"
+image_locations = "https://dancrouse.com/static/img/slider/"
+framename = "slideshow"
+path = "images/"
+
+filenames = glob.glob(os.path.join(path, "*"))
+
+for filename in filenames:
+  print(filename)
+
 while(True): 
     ret, frame = video.read() 
     if ret == True:  
