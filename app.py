90a1ea2e5f7325543f830527199b35e8e64e9ba8
KettleRunner <crousetech@gmail.com>
2021-03-09 08:45:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask, render_template, Response
 import cv2
-
 app = Flask(__name__)
 cap = cv2.VideoCapture(-1)
-cap.set(cv2.CAP_PROP_FPS, 22)
+cap.set(cv2.CAP_PROP_FPS, 20)
 cap.set(3, 640)
 cap.set(4, 480)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-
-
 def gen_frames():
     while True:
         ret, frame = cap.read()  # read the camera frame
@@ -19,8 +16,6 @@ def gen_frames():
             stream_frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + stream_frame + b'\r\n')
-
-
 @app.route('/')
 def index():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
