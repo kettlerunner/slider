e2f3cef89f4baa4b9cb6a3dd8eca91dd5be2c234
KettleRunner <crousetech@gmail.com>
2021-03-08 18:02:30 -0600

    Update cam.py

--- cam.py
+++ cam.py
@@ -31,6 +31,8 @@ while(True):
     print(x)
     if x < 100:
         ret, frame = cap.read()  # read the camera frame
+        cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+        frame = cv2.flip(frame, 1)
         cv2.imshow('Cam', frame)
     elif x > 5000:
         buffer = requests.get(url).text
