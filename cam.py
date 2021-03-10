6a4e6d969bb4e513c1e7cd7377ee4c79c4530d96
KettleRunner <crousetech@gmail.com>
2021-03-09 22:15:10 -0600

    Update cam.py

--- cam.py
+++ cam.py
@@ -1,4 +1,4 @@
-import cv2 
+import cv2
 import numpy as np
 import glob
 import os
@@ -7,91 +7,95 @@ import requests
 import json
 import urllib
 
-bg_frame = cv2.imread("frame.jpg")
-cv2.namedWindow('Cam', cv2.WINDOW_FREERATIO)
-cv2.setWindowProperty('Cam', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-current_width = 0
-current_height = 0
-
-def insert_photo(bg_image, insert_image):
-  bg_width = bg_image.shape[1]
-  bg_height = bg_image.shape[0]
-  in_width = insert_image.shape[1]
-  in_height = insert_image.shape[0]
-  
-  left = int((bg_width - in_width)/2)
-  top = int((bg_height - in_height)/2)
-  
-  overlay_image = bg_image.copy()
-  overlay_image[top:top+in_height, left:left+in_width] = insert_image
-  
-  return overlay_image
-
 url = "https://dancrouse.com/slider"
 local_base = "images/"
 image_locations = "https://dancrouse.com/static/img/slider/"
 framename = "slideshow"
 path = "images/"
 
-max_width = 800
+filenames = glob.glob(os.path.join(path, "*"))
+
+j = 0
+cv2.namedWindow(framename, cv2.WINDOW_FREERATIO)
+frame = cv2.imread("frame.jpg")
+cv2.imshow(framename, frame)
+max_width = 700
 max_height = 480
+scale_x = 0.0
+scale_y = 0.0
 scale = 0.0
 alpha = 1.0
 blurr = 0.0
 
-filenames = glob.glob(os.path.join(path, "*"))
-x = 0
-j = 0
-
-current_img = cv2.imread(filenames[random.randrange(0, len(filenames))])
-previous_img = cv2.imread(filenames[random.randrange(0, len(filenames))])
-scale = min(max_width / previous_img.shape[1],
-                        max_height / previous_img.shape[0])
-scaled_img = cv2.resize(previous_img, (int(previous_img.shape[1] * scale), int(previous_img.shape[0] * scale)))
-current_width = scaled_img.shape[1] 
-current_height = scaled_img.shape[0] 
-slide = insert_photo(bg_frame.copy(), scaled_img)
-cv2.imshow('Cam', slide)
-while(True): 
-    x += 1
-    j += 1
-    print(x)
-    if x > 2000:
-        x = 0
-    elif x == 1:
-        current_width = scaled_img.shape[1] 
-        current_height = scaled_img.shape[0] 
-        slide = insert_photo(bg_frame.copy(), scaled_img)
-        cv2.imshow('Cam', slide)
-    elif x == 1800:
-        previous_img = current_img.copy()
-        current_img = cv2.imread(filenames[random.randrange(0, len(filenames))])
-        buffer = requests.get(url).text
-        server_filenames = json.loads(str(buffer)).get('files')
-        local_filenames = glob.glob(os.path.join(path, "*"))
-        for s in server_filenames:
-            if s not in [i.replace(local_base, "") for i in local_filenames]:
-                urllib.request.urlretrieve(image_locations + s.replace(" ", "%20"), local_base + s.replace(" ", "%20"))
-                filenames = glob.glob(os.path.join(path, "*"))
-        if current_img.shape[1] > max_width or current_img.shape[0] > max_height:
-            scale = min(max_width / current_img.shape[1], max_height / current_img.shape[0])
-            scaled_img = cv2.resize(current_img, (int(current_img.shape[1] * scale), int(current_img.shape[0] * scale))).copy() 
+while True:
+    p_scale = 0
+    x = 0
+    while True:
+        if x == 0:
+            buffer = requests.get(url).text
+            server_filenames = json.loads(str(buffer)).get('files')
+            local_filenames = glob.glob(os.path.join(path, "*"))
+            for s in server_filenames:
+                if s not in [i.replace(local_base, "") for i in local_filenames]:
+                    print(s)
+                    urllib.request.urlretrieve(
+                        image_locations + s.replace(" ", "%20"), local_base + s.replace(" ", "%20"))
+                    filenames = glob.glob(os.path.join(path, "*"))
+            if j == 0:
+                previous_img = cv2.imread(filenames[random.randrange(0, len(filenames))])
+                if previous_img.shape[1] > max_width or previous_img.shape[0] > max_height:
+                    scale = min(max_width / previous_img.shape[1],
+                                max_height / previous_img.shape[0])
+                    previous_img = cv2.resize(
+                        previous_img, (int(previous_img.shape[1] * scale), int(previous_img.shape[0] * scale)))
+                current_img = cv2.imread(filenames[random.randrange(0, len(filenames))])
+                if current_img.shape[1] > max_width or current_img.shape[0] > max_height:
+                    scale = min(max_width / current_img.shape[1], max_height / current_img.shape[0])
+                    current_img = cv2.resize(
+                        current_img, (int(current_img.shape[1] * scale), int(current_img.shape[0] * scale)))
+                next_img = cv2.imread(filenames[random.randrange(0, len(filenames))])
+                if next_img.shape[1] > max_width or next_img.shape[0] > max_height:
+                    scale = min(max_width / next_img.shape[1], max_height / next_img.shape[0])
+                    next_img = cv2.resize(
+                        next_img, (int(next_img.shape[1] * scale), int(next_img.shape[0] * scale)))
+            else:
+                previous_img = current_img.copy()
+                current_img = next_img.copy()
+                next_img = cv2.imread(filenames[random.randrange(0, len(filenames))])
+                if next_img.shape[1] > max_width or next_img.shape[0] > max_height:
+                    scale = min(max_width / next_img.shape[1], max_height / next_img.shape[0])
+                    next_img = cv2.resize(
+                        next_img, (int(next_img.shape[1] * scale), int(next_img.shape[0] * scale)))
+            j += 1
+            current_frame = frame.copy()
+            start_x = int((800 - current_img.shape[1])/2)
+            start_y = int((480 - current_img.shape[0])/2)
+            current_frame[start_y:start_y + current_img.shape[0],
+                          start_x:start_x + current_img.shape[1]] = current_img
+            cv2.imshow(framename, current_frame)
+        elif x > 2500:
+            current_frame = frame.copy()
+            start_x = int((800 - current_img.shape[1])/2)
+            start_y = int((480 - current_img.shape[0])/2)
+            current_frame[start_y:start_y + current_img.shape[0],
+                          start_x:start_x + current_img.shape[1]] = current_img
+            next_frame = frame.copy()
+            start_x = int((800 - next_img.shape[1])/2)
+            start_y = int((480 - next_img.shape[0])/2)
+            next_frame[start_y:start_y + next_img.shape[0],
+                       start_x:start_x + next_img.shape[1]] = next_img
+            alpha = (x - 2500) / 500
+            beta = 1.0 - alpha
+            dst = cv2.addWeighted(current_frame, alpha, next_frame, beta, 0.0)
+            cv2.imshow(framename, dst)
         else:
-            scaled_img = current_img.copy()
-        next_width = scaled_img.shape[1] 
-        next_height = scaled_img.shape[0]
-    elif x > 1900:
-        current_width -= 1
-        current_height -= 1 
-        fade_img = cv2.resize(previous_img, (current_width, current_height))
-        fade_img2 = current_img.copy()
-        slide = insert_photo(bg_frame.copy(), fade_img)
-        slide2 = insert_photo(bg_frame.copy(), fade_img2)
-        image_new = cv2.addWeighted(slide2, (x-1900)/100, slide, 1 - (x-1900)/100, 0)
-        cv2.imshow('Cam', image_new)
-        
-    if cv2.waitKey(1) & 0xFF == ord('s'): 
+            current_frame = frame.copy()
+            start_x = int((800 - current_img.shape[1])/2)
+            start_y = int((480 - current_img.shape[0])/2)
+            current_frame[start_y:start_y + current_img.shape[0],
+                          start_x:start_x + current_img.shape[1]] = current_img
+            cv2.imshow(framename, current_frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        cv2.destroyAllWindows()
         break
- 
-                       
-cv2.destroyAllWindows()
