a1c29c44be988d340268fa7b4930eab284ed3bcb
KettleRunner <crousetech@gmail.com>
2021-03-09 20:27:32 -0600

    Update cam.py

--- cam.py
+++ cam.py
@@ -45,7 +45,7 @@ previous_img = cv2.imread(filenames[random.randrange(0, len(filenames))])
 scale = min(max_width / previous_img.shape[1],
                         max_height / previous_img.shape[0])
 scaled_img = cv2.resize(previous_img, (int(previous_img.shape[1] * scale), int(previous_img.shape[0] * scale)))
-slide = insert_photo(bg_frame, scaled_img)
+slide = insert_photo(bg_frame.copy(), scaled_img)
 cv2.imshow('Cam', slide)
 while(True): 
     x += 1
@@ -70,7 +70,11 @@ while(True):
         else:
             scaled_img = previous_img.copy()
         x = 0
-        slide = insert_photo(bg_frame, scaled_img)
+        slide = insert_photo(bg_frame.copy(), scaled_img)
+        cv2.imshow('Cam', slide)
+    elif x > 1500:
+        scaled_img = cv2.resize(scaled_img, (scaled_img.shape[1] - 1, scaled_img.shape[1] - 1))
+        slide = insert_photo(bg_frame.copy(), scaled_img)
         cv2.imshow('Cam', slide)
                       
     if cv2.waitKey(1) & 0xFF == ord('s'): 
