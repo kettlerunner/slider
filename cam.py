9c1b5d7f25d1dce6071b688494000496235a93d6
KettleRunner <crousetech@gmail.com>
2021-03-16 06:56:39 -0500

    Update cam.py

--- cam.py
+++ cam.py
@@ -106,8 +106,8 @@ while(True):
         else:
             scaled_img = current_img.copy()
     elif x > 1900:
-        if x == 1901:
-            slide_transition = random.randrange(0, 3)
+                if x == 1901:
+            slide_transition = random.randrange(0, 5)
             print(slide_transition)
         if slide_transition == 0:
             current_width -= 1
@@ -133,6 +133,36 @@ while(True):
             image_new[0:int((x-1900)/100.0*800), 0:401] = slide1[-int((x-1900)/100.0*800):480:,0:401]
             image_new[-int((x-1900)/100.0*800):800, 400:801] = slide1[0:int((x-1900)/100.0*800), 400:801]
             cv2.imshow('Cam', image_new)
+        elif slide_transition == 3:
+            fade_img = cv2.resize(previous_img, (current_width, current_height))
+            slide0 = insert_photo(matting0.copy(), fade_img)
+            slide1 = insert_photo(matting1.copy(), scaled_img)
+            image_new = slide0.copy()
+            image_new[0:int((x-1900)/100.0*800), 0:267] = slide1[-int((x-1900)/100.0*800):480:,0:267]
+            image_new[-int((x-1900)/100.0*800):800, 266:533] = slide1[0:int((x-1900)/100.0*800), 266:533]
+            image_new[0:int((x-1900)/100.0*800), 532:800] = slide1[-int((x-1900)/100.0*800):480:,532:800]
+            cv2.imshow('Cam', image_new)
+        elif slide_transition ==  4:
+            fade_img = cv2.resize(previous_img, (current_width, current_height))
+            slide0 = insert_photo(matting0.copy(), fade_img)
+            slide1 = insert_photo(matting1.copy(), scaled_img)
+            i = 0
+            image_new = slide0.copy()
+            y1 = 230
+            y2 = 250
+            while i < 80:
+                image_new[y1:y2, 0:i*10] = slide1[y1:y2, 0:i*10]
+                cv2.imshow('Cam', image_new.copy())
+                if cv2.waitKey(2) & 0xFF == ord('s'): s
+                i += 1
+            i = 0
+            while i < 92:
+                image_new[y1 - int(i/4*23):y2 + int(i/4*23), 0:800] = slide1[y1 - int(i/4*23):y2 + int(i/4*23), 0:800]
+                cv2.imshow('Cam', image_new.copy())
+                if cv2.waitKey(2) & 0xFF == ord('s'): s
+                i += 1
+                
+            x = 2000
         
     if cv2.waitKey(1) & 0xFF == ord('s'): 
         break
