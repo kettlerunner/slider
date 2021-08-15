ff2a433822d8455d5f8979c481039f1f6422d06a
KettleRunner <crousetech@gmail.com>
2021-08-15 07:44:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,11 +24,11 @@ def gen_frames():
             firstFrame = gray
             continue
             
-	frameDelta = cv2.absdiff(firstFrame, gray)
-	    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
-            thresh = cv2.dilate(thresh, None, iterations=2)
-	    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-	    cnts = imutils.grab_contours(cnts)
+        frameDelta = cv2.absdiff(firstFrame, gray)
+	thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.dilate(thresh, None, iterations=2)
+	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	cnts = imutils.grab_contours(cnts)
 
         for c in cnts:
             if cv2.contourArea(c) < args["min_area"]:
