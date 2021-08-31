ea867a3a51f12218941ba6491a5cd0758cd3a539
KettleRunner <crousetech@gmail.com>
2021-08-31 17:57:02 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,6 +1,8 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
+framename = "WatchThis"
+cv2.namedWindow(framename, cv2.WINDOW_FREERATIO)
 max_width = 700
 max_height = 480
 
@@ -12,7 +14,7 @@ while True:
     scale = min(max_width / image.shape[1], max_height / image.shape[0])
     img = cv2.resize(image, (int(image.shape[1] * scale), int(image.shape[0] * scale)))
   
-    cv2.imshow("Video", img)
+    cv2.imshow(framename, img)
     
     if cv2.waitKey(1) & 0xFF ==ord('q'):
         cv2.destroyAllWindows()
