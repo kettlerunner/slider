a3efcf4dcb34550fbbc7c37d832eb7410bbfc427
KettleRunner <crousetech@gmail.com>
2021-08-31 17:40:00 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -3,8 +3,10 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 while True:
-    success, img = cap.read()
-    cv2.imshow("Video", img)
+    success, frame = cap.read()
+    image = cv2.rotate(frame, cv2.cv2.ROTATE_90_CLOCKWISE)
+    image = cv2.flip(image, 1)
+    cv2.imshow("Video", image)
     
     if cv2.waitKey(1) & 0xFF ==ord('q'):
         cv2.destroyAllWindows()
