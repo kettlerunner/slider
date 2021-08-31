20aea4a827ebb233e4e123390fe4f2e4b6c636bd
KettleRunner <crousetech@gmail.com>
2021-08-31 17:59:16 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -12,9 +12,14 @@ while True:
     image = cv2.rotate(frame, cv2.cv2.ROTATE_90_CLOCKWISE)
     image = cv2.flip(image, 1)
     scale = min(max_width / image.shape[1], max_height / image.shape[0])
-    img = cv2.resize(image, (int(image.shape[1] * scale), int(image.shape[0] * scale)))
-  
-    cv2.imshow(framename, img)
+    current_img = cv2.resize(image, (int(image.shape[1] * scale), int(image.shape[0] * scale)))
+    
+    current_frame = frame.copy()
+    start_x = int((800 - current_img.shape[1])/2)
+    start_y = int((480 - current_img.shape[0])/2)
+    current_frame[start_y:start_y + current_img.shape[0],
+                  start_x:start_x + current_img.shape[1]] = current_img
+    cv2.imshow(framename, current_frame)
     
     if cv2.waitKey(1) & 0xFF ==ord('q'):
         cv2.destroyAllWindows()
