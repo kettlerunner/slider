c01cc7b8005d478afd9334f4ee1cde80ce20750d
KettleRunner <crousetech@gmail.com>
2021-08-31 17:47:21 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,13 +1,18 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
+max_width = 700
+max_height = 480
 
 
 while True:
     success, frame = cap.read()
     image = cv2.rotate(frame, cv2.cv2.ROTATE_90_CLOCKWISE)
     image = cv2.flip(image, 1)
-    cv2.imshow("Video", image)
+    scale = min(max_width / previous_img.shape[1], max_height / previous_img.shape[0])
+    img = cv2.resize(image, (int(image.shape[1] * scale), int(image.shape[0] * scale)))
+  
+    cv2.imshow("Video", img)
     
     if cv2.waitKey(1) & 0xFF ==ord('q'):
         cv2.destroyAllWindows()
