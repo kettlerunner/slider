aa31ba448ff42dbe6160ff8d74cc24b7bca029f7
KettleRunner <crousetech@gmail.com>
2021-08-31 17:47:47 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -9,7 +9,7 @@ while True:
     success, frame = cap.read()
     image = cv2.rotate(frame, cv2.cv2.ROTATE_90_CLOCKWISE)
     image = cv2.flip(image, 1)
-    scale = min(max_width / previous_img.shape[1], max_height / previous_img.shape[0])
+    scale = min(max_width / image.shape[1], max_height / image.shape[0])
     img = cv2.resize(image, (int(image.shape[1] * scale), int(image.shape[0] * scale)))
   
     cv2.imshow("Video", img)
