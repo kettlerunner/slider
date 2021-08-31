ad81a780df52beff75d03c260cd968715da587f1
KettleRunner <crousetech@gmail.com>
2021-08-31 18:02:45 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -3,6 +3,8 @@ import cv2
 cap = cv2.VideoCapture(0)
 framename = "WatchThis"
 cv2.namedWindow(framename, cv2.WINDOW_FREERATIO)
+frame = cv2.imread("frame.jpg")
+cv2.imshow(framename, frame)
 max_width = 700
 max_height = 480
 
@@ -13,7 +15,6 @@ while True:
     image = cv2.flip(image, 1)
     scale = min(max_width / image.shape[1], max_height / image.shape[0])
     current_img = cv2.resize(image, (int(image.shape[1] * scale), int(image.shape[0] * scale)))
-    
     current_frame = frame.copy()
     start_x = int((800 - current_img.shape[1])/2)
     start_y = int((480 - current_img.shape[0])/2)
