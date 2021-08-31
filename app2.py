c7856abdf01220deaa5e6ec6ddcbec852752dce6
KettleRunner <crousetech@gmail.com>
2021-08-31 18:04:39 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -5,8 +5,8 @@ framename = "WatchThis"
 cv2.namedWindow(framename, cv2.WINDOW_FREERATIO)
 frame = cv2.imread("frame.jpg")
 cv2.imshow(framename, frame)
-max_width = 700
-max_height = 480
+max_width = 600
+max_height = 300
 
 
 while True:
@@ -15,12 +15,7 @@ while True:
     image = cv2.flip(image, 1)
     scale = min(max_width / image.shape[1], max_height / image.shape[0])
     current_img = cv2.resize(image, (int(image.shape[1] * scale), int(image.shape[0] * scale)))
-    current_frame = frame.copy()
-    start_x = int((800 - current_img.shape[1])/2)
-    start_y = int((480 - current_img.shape[0])/2)
-    current_frame[start_y:start_y + current_img.shape[0],
-                  start_x:start_x + current_img.shape[1]] = current_img
-    cv2.imshow(framename, current_frame)
+    cv2.imshow(framename, current_img)
     
     if cv2.waitKey(1) & 0xFF ==ord('q'):
         cv2.destroyAllWindows()
